7d62572aec3188d5d112cb2c53cb22cefc730997
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2024-02-15 11:23:53 +0530

    🚑️ v5.1 - Fixed production error + updated package-lock.json

--- src/microservices/teams/teams.module.ts
+++ src/microservices/teams/teams.module.ts
@@ -4,11 +4,10 @@ import { TeamsService } from './services';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Team } from './entities';
 import { UsersModule } from '../users/users.module';
-import { TeamOwnerGuard } from 'src/guards/teamOwner.guard';
 
 @Module({
   imports: [UsersModule, TypeOrmModule.forFeature([Team])],
   controllers: [TeamsController],
-  providers: [TeamsService, TeamOwnerGuard],
+  providers: [TeamsService],
 })
 export class TeamsModule {}
